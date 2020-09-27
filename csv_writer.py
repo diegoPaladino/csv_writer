bfdfde5e634131aafbe12f0399ba490424626484
diegoPaladino <diegopaladinofoto@gmail.com>
2020-09-27 12:28:08 -0300

    202009271228

--- csv_writer.py
+++ csv_writer.py
@@ -1,2 +1,20 @@
 #csv_writer
+#UNSUCESSFUL - but was sucessful in the Jupyter notebook ()
 
+#libraries
+import csv
+import investpy as inv
+
+
+#declarations
+
+
+#execution
+lista = inv.get_stocks('brazil')
+
+# with open('arquivos/saida.csv','w',newline='') as f:
+#     thewriter = csv.writer(f)
+
+#     thewriter.writerow(lista)
+
+lista.to_excel('lista.xlsx',encoding='utf-8',index=False)
